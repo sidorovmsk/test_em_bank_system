9f36ff716bc576a8b8adaf42bcf718d915ad8b94
a.v.sidorov <a.v.sidorov@itfbgroup.ru>
2024-01-10 13:25:46 +0300

    add DataLoader

---
+++ src/main/java/spring_boot_java/test_em/components/DataLoader.java
@@ -0,0 +1,31 @@
+package spring_boot_java.test_em.components;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import spring_boot_java.test_em.models.ERole;
+import spring_boot_java.test_em.models.Role;
+import spring_boot_java.test_em.repositories.RoleRepository;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    private final RoleRepository roleRepository;
+
+    public DataLoader(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role userRole = new Role();
+        userRole.setName(ERole.ROLE_USER);
+        Role adminRole = new Role();
+        adminRole.setName(ERole.ROLE_ADMIN);
+        Role modRole = new Role();
+        modRole.setName(ERole.ROLE_MODERATOR);
+
+        roleRepository.save(userRole);
+        roleRepository.save(adminRole);
+        roleRepository.save(modRole);
+    }
+}
+
